fedabe851f606ad8c373af0bc1d763808ce49994
OliverFitzpatrick18 <oliverkfitzpatrick@gmail.com>
2024-11-08 14:18:43 +0000

    Assessment 3

---
+++ grades.py
@@ -0,0 +1,14 @@
+student_mark = int(input("Enter your mark here = "))
+
+if student_mark <= 100 and student_mark >= 70:
+    print("Your grade is A")
+elif student_mark <=69 and student_mark >=60:
+    print("your grade is B")
+elif student_mark <=59 and student_mark >=50:
+    print("your grade is C")
+elif student_mark <=49 and student_mark >=40:
+    print("your grade is D")
+elif student_mark <=39 and student_mark >=30:
+    print("your grade is E")
+elif student_mark <=29 and student_mark >=20:
+    print("your grade is F")
