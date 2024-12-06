c65daee55d09569c139b5a9bb7dcc0e5fa87829f
OliverFitzpatrick18 <oliverkfitzpatrick@gmail.com>
2024-12-06 13:34:57 +0000

    Assessment 5

---
+++ Listops.py
@@ -0,0 +1,26 @@
+
+Integer = [6, 53, 96, 83, 20, 2, 7, 34, 17, 30]
+
+biggest_num = max(Integer)
+
+smallest_num = min(Integer)
+
+
+
+
+
+
+
+print(Integer)
+
+print(sum(Integer))
+
+print(biggest_num)
+
+Integer.reverse()
+
+print(Integer)
+
+print(smallest_num)
+
+
